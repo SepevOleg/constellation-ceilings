cd7b724bb8711f1bd8a985818f3cea0199f8c2ec
SepevOleg <107648948+SepevOleg@users.noreply.github.com>
2022-10-21 20:27:43 +0300

    2

--- js/form.js
+++ js/form.js
@@ -9,7 +9,7 @@ $("form").submit(function() { // Событие отправки с формы
 var form = $(this).serialize(); // Собираем данные из полей
 $.ajax({
 type: "POST", 
-url: "../form/sendform.php",
+url: "/form/sendform.php",
 data: form,
 success: function() {
 $('.popup').addClass('active');
