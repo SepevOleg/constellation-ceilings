abfad5be6d543d28f6d10c567009e8867a30054a
Aleksey <fosterfosterfoster63@gmail.com>
2022-10-25 20:33:47 +0400

    save fix slider

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,12 @@
 let headerMenuBtn = document.querySelector('.header-menu_btn');
 let headerTopmenu = document.querySelector('.header-hamburger');
 headerMenuBtn.addEventListener('click', function(){
-	headerMenuBtn.classList.toggle('active');
+	console.log(headerTopmenu)
 	headerTopmenu.classList.toggle('active');
+	// headerTopmenu.classList.toggle('active');
+	headerTopmenu.addEventListener('click', () => {
+		console.log(headerTopmenu)
+		headerTopmenu.classList.remove('active');
+	})
+	// headerTopmenu.classList.toggle('active');
 })
