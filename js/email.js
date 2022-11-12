cc795fd3d31fa5b557b3095b37f33d10f21e48e5
Aleksey <fosterfosterfoster63@gmail.com>
2022-11-12 19:07:52 +0400

    change form work

--- js/email.js
+++ js/email.js
@@ -1,3 +1,17 @@
+var templateName = ''
+$('.open-modal').click(function(event){
+  var template = event.target.parentNode.parentNode
+  templateName = template.querySelector('.template-name')
+  if (templateName) {
+    templateName = templateName.querySelector('p').textContent
+  }
+  $('.block-popup, .overlay').fadeIn();
+})
+$('.block-popup span').click(function(event){
+  templateName = ''
+  $('.block-popup, .overlay').fadeOut();
+})
+const confAccept = document.getElementById('confAccept')
 const form = document.getElementById('form')
 const formBtn = document.getElementById('sendEmail')
 const squareQty = document.getElementById('number')
@@ -10,20 +24,64 @@ const inputs = {
 }
 const username = document.getElementById('userName');
 const phone = document.getElementById('userPhone');
+var templateNameValue = document.getElementById('templateName')
 if (form) {
-  formBtn.addEventListener('click', function(event) {
-    event.preventDefault();
-    costValue.value = cost.textContent
-    squareQtyValue.value = square.textContent
-    console.log(squareQty.textContent, cost.textContent)
-    emailjs.sendForm('service_rl5mcci', 'template_zszpsl7', form)
-    .then(function() {
-      alert('Ваши данные приняты, ждите ответной связи')
-      console.log('SUCCESS!');
-      form.reset()
-    }, function(error) {
-      alert(error)
-      console.log('FAILED...', error);
+    formBtn.addEventListener('click', function(event) {
+      if (confAccept.checked) {
+        console.log(confAccept.checked)
+        event.preventDefault();
+        costValue.value = cost.textContent
+        squareQtyValue.value = square.textContent
+        console.log(squareQty.textContent, cost.textContent)
+        if (!templateName) {
+          emailjs.sendForm('service_rl5mcci', 'template_zszpsl7', form)
+          .then(function() {
+            alert('Ваши данные приняты, ждите ответной связи')
+            console.log('SUCCESS!');
+            form.reset()
+          }, function(error) {
+            alert(error)
+            console.log('FAILED...', error);
+          });
+        } else {
+          templateNameValue.value = templateName
+          console.log(templateNameValue)
+          emailjs.sendForm('service_rl5mcci', 'template_4fth5g8', form)
+          .then(function() {
+            alert('Ваши данные приняты, ждите ответной связи')
+            console.log('SUCCESS!');
+            form.reset()
+          }, function(error) {
+            alert(error)
+            console.log('FAILED...', error);
+          });
+        }
+      } else {
+        event.preventDefault();
+        return 
+      }
+      
+      
     });
-  });
+    
+
+  // } else {
+  //   templateNameValue.value = templateName
+  //   console.log(templateNameValue)
+  //   formBtn.addEventListener('click', function(event) {
+  //     event.preventDefault();
+  //     costValue.value = cost.textContent
+  //     squareQtyValue.value = square.textContent
+  //     emailjs.sendForm('service_rl5mcci', 'template_4fth5g8', form)
+  //     .then(function() {
+  //       alert('Ваши данные приняты, ждите ответной связи')
+  //       console.log('SUCCESS!');
+  //       form.reset()
+  //     }, function(error) {
+  //       alert(error)
+  //       console.log('FAILED...', error);
+  //     });
+  //   });
+  // }
+  
 }
--- js/form.js
+++ js/form.js
@@ -1,9 +1,4 @@
-$('.open-modal').click(function(){
-  $('.block-popup, .overlay').fadeIn();
-})
-$('.block-popup span').click(function(){
-  $('.block-popup, .overlay').fadeOut();
-})
+
 // $(document).ready(function(){
 // $("form").submit(function() { // Событие отправки с формы
 //   var form = $(this).serialize(); // Собираем данные из полей
