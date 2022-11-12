2658bd79d09cb96275a661eabca3cbf951c9da30
Aleksey <fosterfosterfoster63@gmail.com>
2022-11-12 18:15:35 +0400

    add inputs

--- js/email.js
+++ js/email.js
@@ -1,5 +1,9 @@
 const form = document.getElementById('form')
 const formBtn = document.getElementById('sendEmail')
+const squareQty = document.getElementById('number')
+const cost = document.getElementById('rezultatRange')
+const costValue = document.getElementById('costValue')
+const squareQtyValue = document.getElementById('squareQtyValue')
 const inputs = {
   user_name: 'aleksey',
   contact_number: 'nomer'
@@ -9,12 +13,9 @@ const phone = document.getElementById('userPhone');
 if (form) {
   formBtn.addEventListener('click', function(event) {
     event.preventDefault();
-    console.log(username.value, phone.value)
-    const data = {
-      user_name: username.value,
-      contact_number: phone.value
-    }
-    console.log(emailjs.sendForm)
+    costValue.value = cost.textContent
+    squareQtyValue.value = square.textContent
+    console.log(squareQty.textContent, cost.textContent)
     emailjs.sendForm('service_rl5mcci', 'template_zszpsl7', form)
     .then(function() {
       alert('Ваши данные приняты, ждите ответной связи')
