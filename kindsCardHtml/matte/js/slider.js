68eb1bee596799c09791bbd64c987d7878c61cee
SepevOleg <107648948+SepevOleg@users.noreply.github.com>
2022-11-30 20:40:40 +0300

    salle

--- kindsCardHtml/matte/js/slider.js
+++ kindsCardHtml/matte/js/slider.js
@@ -1,20 +1,3 @@
-const slider = document.querySelector("main-slider");
-const container = document.querySelector("main-slider_box");
-const slides = document.querySelector("main-slider_box__slide");
-const navigations = document.querySelectorAll("main-slider_box__navigation");
-
-let active0rder = 0;
-
-function init() {
-  for (let i = 0; i < slides.length; i++) {
-    const slide = slides[i];
-
-    slide.dataset.order = i;
-  }
-
-  active0rder = Math.floor(slides.length / 2);
-
-  update();
-}
-
-function update() {}
+$(document).ready(function () {
+  $('.sliders').slick()
+});
